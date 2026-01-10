bbf5f52d02d12c6ed1cc19c5e45df16169063136
Jaysus91 <jadenshamid@gmail.com>
2026-01-11 00:13:10 +0100

    making progress

--- task_tracker/task_tracker.py
+++ task_tracker/task_tracker.py
@@ -1,5 +1,16 @@
 from typing import TextIO
 from io import StringIO
 
-
-task = input("What task do you have? ")
+while True:
+    task = input("What task do you have? (type 'exit' to quit): ")
+    task = task.lower()
+    tracker_file = task
+    if task == "exit":
+        print('Thank you')
+        break
+    try:
+        with open(tracker_file, 'w') as file:
+            file.write(task)
+            print('Sucessfully recorded task')
+    except FileNotFoundError:
+        print('File not found')
